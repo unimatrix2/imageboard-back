33d14c7a48efa750263f1404f6e309a8f4633538
unimatrix2 <dnny.nis@gmail.com>
2021-05-20 20:04:58 -0300

    removed next stuff & unused items

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import next from '../app';
 import tokenRoutes from './token/token.routes';
-import userRoutes from './user/user.routes';
 import boardRoutes from './board/board.routes';
 
 const router = Router();
 
-
-router.get('/', (req, res) => {
-    next.render(req, res, '/');
-});
-
 router.use('/token', tokenRoutes);
 router.use('/board', boardRoutes);
-router.use('/', userRoutes);
 export default router;
