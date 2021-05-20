73d67ed60493f42c999f8d970ad50ebe076e48b9
unimatrix2 <dnny.nis@gmail.com>
2021-05-20 20:05:36 -0300

    linted

--- src/routes/token/token.routes.js
+++ src/routes/token/token.routes.js
@@ -5,24 +5,28 @@ import { createAnon, verifyUserToken } from '../../services/user.service';
 const router = Router();
 
 router.get('/verify', async (req, res) => {
-    try {
-        // Get token from headers
-        const token = req.signedCookies.token;
-        // validate token
-        const newToken = await verifyUserToken(token);
-        // Send token
-        res.cookie('token', newToken.auth, { maxAge: process.env.COOKIE_EXPIRY, httpOnly: true, signed: true, sameSite: 'strict', secure: true })
-        res.json({ nick: newToken.nick, message: 'Validated', type: 'Success', status: 200 }).status(200);
-
-    } catch (error) {
-        // Create new anon & send (user will need sensitive info at the start)
-        try {
-            const newAnon = await createAnon();
-            res.cookie('token', newAnon.token, { maxAge: process.env.COOKIE_EXPIRY, httpOnly: true, signed: true, sameSite: 'strict', secure: true })
-            res.json(newAnon.anon).status(200);
-        } catch (error) { throw new AppError(error) };
-        
-    };
+	try {
+		// Get token from headers
+		const { token } = req.signedCookies;
+		// validate token
+		const newToken = await verifyUserToken(token);
+		// Send token
+		res.cookie('token', newToken.auth, {
+			maxAge: process.env.COOKIE_EXPIRY, httpOnly: true, signed: true, sameSite: 'strict', secure: true,
+		});
+		res.json({
+			nick: newToken.nick, message: 'Validated', type: 'Success', status: 200,
+		}).status(200);
+	} catch (error) {
+		// Create new anon & send (user will need sensitive info at the start)
+		try {
+			const newAnon = await createAnon();
+			res.cookie('token', newAnon.token, {
+				maxAge: process.env.COOKIE_EXPIRY, httpOnly: true, signed: true, sameSite: 'strict', secure: true,
+			});
+			res.json(newAnon.anon).status(200);
+		} catch (err) { throw new AppError(err); }
+	}
 });
 
 export default router;
