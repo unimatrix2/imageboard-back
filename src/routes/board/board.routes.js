97b4b5c93c262cd032b5c4513f8cc5b998d7f803
unimatrix2 <dnny.nis@gmail.com>
2021-05-20 20:05:17 -0300

    removed next stuff

--- src/routes/board/board.routes.js
+++ src/routes/board/board.routes.js
@@ -1,17 +1,15 @@
 import { Router } from 'express';
-import { createBoard, getBoard } from '../../services/board.service';
-import next from '../../app';
+import { getBoard } from '../../services/board.service';
+
 const router = Router();
 
 router.get('/:abbr', async (req, res) => {
-    try {
-        const board = await getBoard(req.params.abbr);
-        next.render(req, res, `/${req.params.abbr}`, { board });
-    } catch (error) {
-        res.status(500).json(error);
-    }
-    
-    //next.render(req, res, `/${req.params.body}`, { board });
+	try {
+		const board = await getBoard(req.params.abbr);
+		res.status(200).json(board);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 export default router;
