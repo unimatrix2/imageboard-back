6e7eec054113d52ddeadd57d900ea7960590f6a3
unimatrix2 <dnny.nis@gmail.com>
2021-05-21 16:48:02 -0300

    board secret now encrypted

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -1,9 +1,13 @@
 import { create, get } from '../repositories/board.repository';
+import { encrypt } from '../utils/passwordManager';
 import AppError from '../errors/AppError';
 
 export const createBoard = async (body) => {
 	try {
-		const board = await create(body);
+		const board = await create({
+			...body,
+			secret: await encrypt(body.secret),
+		});
 		return board;
 	} catch (error) {
 		throw new AppError(error);
