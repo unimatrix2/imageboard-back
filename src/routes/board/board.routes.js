c6ac7228fba3723feb72fe1ebbece33be6187604
unimatrix2 <dnny.nis@gmail.com>
2021-05-21 16:47:35 -0300

    added board creation route

--- src/routes/board/board.routes.js
+++ src/routes/board/board.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { getBoard } from '../../services/board.service';
+import { getBoard, createBoard } from '../../services/board.service';
+import { verifyToken } from '../../utils/passwordManager';
 
 const router = Router();
 
@@ -7,6 +8,18 @@ router.get('/:abbr', async (req, res) => {
 	try {
 		const board = await getBoard(req.params.abbr);
 		res.status(200).json(board);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.post('/create', async (req, res) => {
+	try {
+		const { body } = req;
+		const { token } = req.signedCookies;
+		const founder = await verifyToken(token);
+		const newBoard = await createBoard({ ...body, founder: founder.id, modmins: [founder.id] });
+		res.status(200).json(newBoard);
 	} catch (error) {
 		res.status(500).json(error);
 	}
